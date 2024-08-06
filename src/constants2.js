064196aaab248b40cbc9ca960542befd30968461
Ritika Nankar <112073087+Ritika128@users.noreply.github.com>
2024-08-06 16:43:50 +0530

    Update constants2.js

--- src/constants2.js
+++ src/constants2.js
@@ -1,2 +1,2 @@
-export const pinataApiKey = "19a1db679e54fdc823b5";
-export const pinataSecretApiKey ="a08adcced70b1540dd396895bd506c901cc453b1c3900906eef2bf6b16f7bca5";
+export const pinataApiKey = "YOUR_PINATA_API_KEY";
+export const pinataSecretApiKey ="YOUR_PINATA_SECRET_API_KEY";
